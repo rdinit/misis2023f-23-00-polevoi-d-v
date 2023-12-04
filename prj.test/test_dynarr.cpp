82a3f714d1b7f08e44af3f930c5128adb7ac8eee
dvpsun <dvpsun@gmail.com>
2023-12-04 07:11:06 +0300

    fix classes interface

--- prj.test/test_dynarr.cpp
+++ prj.test/test_dynarr.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-#include <cstdint>
+#include <cstddef>
 
 class DynArr {
   public:
--- prj.test/test_rational.cpp
+++ prj.test/test_rational.cpp
@@ -2,18 +2,63 @@
 #include "doctest.h"
 
 #include <cstdint>
+#include <iosfwd>
 
 class Rational {
   public:
-    Rational(const int64_t num = 0, const int64_t den = 1);
-    int64_t num() const { return num_;  }
-    int64_t den() const { return den_; }
+    Rational() = default;
+    Rational(const Rational&) = default;
+    Rational(const std::int64_t num) noexcept : num_(num) { }
+    Rational(const std::int64_t num, const std::int64_t den);
+    ~Rational() = default;
+    [[nodiscard]] Rational& operator=(const Rational&) = default;
+
+    [[nodiscard]] std::int64_t num() const noexcept { return num_;  }
+    [[nodiscard]] std::int64_t den() const noexcept { return den_; }
+
+    [[nodiscard]] bool operator==(const Rational& rhs) const noexcept;
+    [[nodiscard]] bool operator!=(const Rational& rhs) const noexcept;
+    [[nodiscard]] bool operator<(const Rational& rhs) const noexcept;
+    [[nodiscard]] bool operator<=(const Rational& rhs) const noexcept;
+    [[nodiscard]] bool operator>(const Rational& rhs) const noexcept;
+    [[nodiscard]] bool operator>=(const Rational& rhs) const noexcept;
+
+    [[nodiscard]] Rational operator-() const noexcept { return {-num_, den_}; }
+
+    Rational& operator+=(const Rational& rhs) noexcept;
+    Rational& operator-=(const Rational& rhs) noexcept;
+    Rational& operator*=(const Rational& rhs) noexcept;
+    Rational& operator/=(const Rational& rhs);
+
+  //! \brief Форматированный вывод в поток ostrm рационального числа в виде num/den.
+  //!
+  //! \param[in,out] ostrm - поток
+  //! \retval поток
+  [[nodiscard]] std::ostream& WriteTo(std::ostream& ostrm) const noexcept;
+
+  //! \brief Форматированный ввод из потока istrm комплексного числа в виде num/den.
+  //!
+  //! \param[in,out] istrm - поток
+  //! \retval поток
+  [[nodiscard]] std::istream& ReadFrom(std::istream& istrm) noexcept;
+
 private:
-    int64_t num_ = 0;
-    int64_t den_ = 1;
+    std::int64_t num_ = 0; //!< числитель
+    std::int64_t den_ = 1; //!< знаменатель, > 0
 };
 
-Rational::Rational(const int64_t num, const int64_t den)
+Rational operator+(const Rational& lhs, const Rational& rhs) noexcept;
+Rational operator-(const Rational& lhs, const Rational& rhs) noexcept;
+Rational operator*(const Rational& lhs, const Rational& rhs) noexcept;
+Rational operator/(const Rational& lhs, const Rational& rhs);
+
+std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs) noexcept;
+
+std::istream& operator>>(std::istream& ostrm, Rational& rhs) noexcept;
+
+
+
+Rational::Rational(const std::int64_t num, const std::int64_t den)
   : num_(num), den_(den) {
   if (0 == den_) {
     throw std::invalid_argument("Zero denumenator in Rational ctor");
